2008a083bc51fb0b39e6006459d1436048da4b23
israel386 <israel_delarosa@knights.ucf.edu>
2022-05-15 20:39:53 -0400

    updated script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,19 +1,61 @@
-var timer = 60;
-var questionN = 0;
-var timeInt;
-
 var timeEl = document.querySelector("#timer");
-var startEl = document.querySelector("#start-quiz");
 var question = document.querySelector("#question");
-var choices = Array.from(document.querySelector(".choice-text"));
+var choices = Array.from(document.querySelectorAll(".choice-text"));
 
-var startButton = function (event) {
-    timerCountDown()
-}
+let currentQuestion = {};
+let acceptingAnswers = true;
+var timeLeft = 50;
+let questionCounter = 0;
+let availableQuestions = [];
 
-function timerCountDown() {
-    var timeLeft = 60;
+let questions = [
+    {
+        question: "A very useful tool used during development and debugging for printing content to the degugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        answer: 4,
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: 3,
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with __________.",
+        choice1: "Quotes",
+        choice2: "curly brackets",
+        choice3: "paraenthesis",
+        choice4: "square brackets",
+        answer: 3,
+    },
+    {
+        question: "String values must be enclosed within __________ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "paranthesis",
+        answer: 3,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store __________.",
+        choice1: "number and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4,
+    }
+];
 
+const MAX_QUESTIONS = 5;
+
+startQuiz = () => {
+    questionCounter = 0;
+    availableQuestions = [...questions];
     var timeInterval = setInterval(function () {
         if (timeLeft >= 1) {
             timeEl.textContent = "Time: " + timeLeft;
@@ -22,28 +64,61 @@ function timerCountDown() {
         else if (timeLeft === 0) {
             timeEl.textContent = "Time: " + timeLeft;
             clearInterval(timeInterval);
-            // when the timer runs out the quiz ends
             endQuiz();
         }
-        if (timeleft === 0) {
+        if (timeLeft === 0) {
 
         }
     }, 1000);
+
+    getNewQuestion();
 };
-timerCountDown();
 
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
+        return window.location.assign('/end.html')
+    };
 
+    questionCounter++
 
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
 
+    availableQuestions.splice(questionsIndex, 1);
 
+    acceptingAnswers = true;
+};
 
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return
 
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
 
+        let classToApply = selectedAnswer == currentQuestion.anwer ? "correct" : "incorrect";
+        if (classToApply === "incorrect") {
+            timeLeft = timeLeft - 10;
+        }
 
+        selectedChoice.parentElement.classList.add(classToApply);
 
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
 
+    });
+});
 
+startQuiz();
 
-// startEl.addEventListener("click", startButton);
